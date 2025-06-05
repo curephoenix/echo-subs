a87f34cc40cc11992006baa9fb00a460a1d0dcdb
Cynthia <46873955+curephoenix@users.noreply.github.com>
2025-06-05 02:55:28 -0400

    Usotsuki wa Tomorrow no Hajimari (Maybe)

---
+++ PriPara/setup.py
@@ -0,0 +1,33 @@
+from muxtools import *
+setup = Setup(
+    f"{74}",
+     None,
+    bdmv_dir=f"./BDMV/",
+    show_name="PriPara",
+    out_name=R"$show$ - E$ep$ (BD 1080p) [Echo-Subs]",
+    mkv_title_naming=R"$show$ - E$ep$ - $title$",
+    out_dir="muxed",
+    clean_work_dirs=False
+)
+
+video_file = GlobSearch(f"PriPara - {setup.episode}*.mkv", dir="./")
+premux = Premux(video_file, subtitles=None, keep_attachments=False, mkvmerge_args=["--no-global-tags", "--no-chapters"])
+subtitle = SubFile(GlobSearch("*_dialogue.ass", allow_multiple=True, dir=f"./{setup.episode}/"))
+chapters = Chapters.from_sub(subtitle, use_actor_field=True)
+subtitle.clean_garbage().clean_extradata().set_headers(
+    (ASSHeader.PlayResX, 1920),
+    (ASSHeader.PlayResY, 1080),
+    (ASSHeader.LayoutResX, 1920),
+    (ASSHeader.LayoutResY, 1080),
+    (ASSHeader.ScaledBorderAndShadow, True),
+    (ASSHeader.WrapStyle, 2),
+    ("Title", "Echo-subs")
+)
+fonts = subtitle.collect_fonts()
+mux(
+    premux,
+    subtitle.to_track("English", "en"),
+    *fonts,
+    chapters,
+    tmdb=TmdbConfig(67627)
+)
